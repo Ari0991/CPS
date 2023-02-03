c912b4f6aea34a850200e99aec26a5744c8e84b0
Tatiana Vostrikova <t.belgorodskaya@yandex.ru>
2023-02-03 23:16:47 +0700

    fix a stupid mistake with js file name

---
+++ swiper.js
@@ -0,0 +1,68 @@
+// import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
+
+const swiper = new Swiper(".swiper", {
+  enabled: true,
+  observer: true,
+  direction: "horizontal",
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  clickable: true,
+  breakpoints: {
+    768: {
+      enabled: false,
+    },
+    1120: {
+      enabled: false,
+    },
+  },
+
+  pagination: {
+    el: ".swiper__pagination",
+    clickable: true,
+  },
+});
+
+// Article button click
+
+const article = document.querySelector(".article");
+const readMoreArticle = article.querySelector(".button--read-more");
+let readMoreArticleText = readMoreArticle.addEventListener(
+  "click",
+  function () {
+    const articleText = article.querySelector(".article__text");
+    let paragraph = document.createElement("p");
+    paragraph.textContent =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quam a, officia similique unde deserunt. Pariatur, et adipisci in nisi, fuga assumenda nobis doloremque sint quod sequi voluptatum similique omnis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quam a, officia similique unde deserunt. Pariatur, et adipisci in nisi, fuga assumenda nobis doloremque sint quod sequi voluptatum similique omnis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quam a, officia similique unde deserunt. Pariatur, et adipisci in nisi, fuga assumenda nobis doloremque sint quod sequi voluptatum similique omnis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quam a, officia similique unde deserunt. Pariatur, et adipisci in nisi, fuga assumenda nobis doloremque sint quod sequi voluptatum similique omnis.";
+    articleText.appendChild(paragraph);
+    readMoreArticle.classList.add("hidden");
+  }
+);
+
+// Swiper button click
+
+const swiperBlock = document.querySelector(".swiper__block");
+const swiperWrapper = swiperBlock.querySelector(".swiper-wrapper");
+const readMore = swiperBlock.querySelector(".swiper__button--read-more");
+const buttonIcon = readMore.querySelector(".button__icon");
+const readMoreText = readMore.querySelector(".button__text");
+
+let readMoreContent = readMore.addEventListener("click", function () {
+  let isContentHide = swiperWrapper.classList.toggle(
+    "swiper-wrapper--hide-content"
+  );
+  if (!isContentHide) {
+    swiperWrapper.classList.remove("swiper-wrapper--hide-content");
+    swiperWrapper.classList.add("swiper-wrapper--max-height");
+    if (window.innerWidth >= 1120) {
+      readMoreText.textContent = "Скрыть";
+      buttonIcon.style.transform = "rotate(180deg)";
+    } else {
+      readMore.classList.add("hidden");
+    }
+  } else if (isContentHide) {
+    swiperWrapper.classList.add("swiper-wrapper--hide-content");
+    swiperWrapper.classList.remove("swiper-wrapper--max-height");
+    readMoreText.textContent = "Показать все";
+    buttonIcon.style.transform = "rotate(0deg)";
+  }
+});
